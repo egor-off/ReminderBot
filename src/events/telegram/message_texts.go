3cafbc049525dd6b278cebeb97735a03239244f5
egor-off <egor-off3@ya.ru>
2024-04-17 14:17:55 +0300

    messages_texts fix

--- src/events/telegram/message_texts.go
+++ src/events/telegram/message_texts.go
@@ -11,7 +11,7 @@ Press the "` + randomText + `" button and you'll get random link from messages y
 
 	msgDeafult = "Push the button to get the result ⏬"
 
-	msgUnknownCommand = "Unknown command 🫠\n\nPlease, send me a link to save.\nOr you can " + strings.ToLower(msgDeafult)
+	msgUnknownCommand = "Unknown command 🫠\n\nPlease, send me a link to save.\nOr " + strings.ToLower(msgDeafult)
 	msgNoSavedURL = "No saved URL 😬\n\n" + msgDeafult
 	msgAllreadyExists = "This URL already exists 🤌🏽\n\n" + msgDeafult
 	msgSaved = "Saved 🤞🏻\n\n" + msgDeafult
